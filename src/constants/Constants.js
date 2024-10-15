7e85666b110542f508dc6884a2faa8f5b8a49197
ANHDUNG\padun <padungg8@gmail.com>
2024-10-15 23:55:18 +0700

    update

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,3 +1,3 @@
-export const API_ENDPOINT_SERVER = 'http://localhost:5000/api'; 
-export const URL_ENDPOINT_SERVER = 'http://localhost:5000/'; 
-export const URL_CLIENT = 'http://localhost:3000'; 
+export const API_ENDPOINT_SERVER = 'http://34.1.205.102:5000/api'; 
+export const URL_ENDPOINT_SERVER = 'http://34.1.205.102:5000/'; 
+export const URL_CLIENT = 'http://34.1.205.102:3309'; 
